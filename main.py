653019b786bafc1f721dbf501d3354a14428e017
tsar <your.email@example.com>
2020-02-12 22:01:38 +0400

    Обновление кнопок

--- main.py
+++ main.py
@@ -19,10 +19,8 @@ myvar_set_left.set('0')
 myvar_set_right.set('0')
 
 
-
-def UpdateAll():
-    
-    #Обновляем сеты команд
+def UpdateSpin():
+#Обновляем сеты команд
     set1 = spin_left.get()
     my_file = open("set1.txt", "w")
     my_file.write(set1)
@@ -32,19 +30,7 @@ def UpdateAll():
     my_file = open("set2.txt", "w")
     my_file.write(set2)
     my_file.close()
-
-    #Обновляем название команд
-    team_left = message_left.get()
-    my_file = open("team1.txt", "w")
-    my_file.write(team_left)
-    my_file.close()
-
-    team_right = message_right.get()
-    my_file = open("team2.txt", "w")
-    my_file.write(team_right)
-    my_file.close()
-
-    #Обновляем количество очков
+#Обновляем количество очков
     team_score_left = spin_big_left.get()
     my_file = open("score1.txt", "w")
     my_file.write(team_score_left)
@@ -54,6 +40,18 @@ def UpdateAll():
     team_score_right = spin_big_right.get()
     my_file = open("score2.txt", "w")
     my_file.write(team_score_right)
+    my_file.close()
+
+def UpdateConnandName():
+    #Обновляем название команд
+    team_left = message_left.get()
+    my_file = open("team1.txt", "w", encoding='utf-8')
+    my_file.write(team_left)
+    my_file.close()
+
+    team_right = message_right.get()
+    my_file = open("team2.txt", "w", encoding='utf-8')
+    my_file.write(team_right)
     my_file.close()
 
 
@@ -91,7 +89,7 @@ def ResetScore():
 
 
 # Кнопки и их позиционирование
-btn_update_all = Button(window, text="Обновить все", bg="white", fg="blue", command=UpdateAll)
+btn_update_all = Button(window, text="Обновить название команд", bg="white", fg="blue", command = UpdateConnandName)
 btn_update_all.place(x=50, y=300, relwidth=0.2, relheight=0.1)
 
 btn_reset_score = Button(window, text="Сбросить очки", bg="white", fg="blue", comman = ResetScore)
@@ -107,11 +105,11 @@ btn_reset_set.place(x=450, y=300, relwidth=0.2, relheight=0.1)
 # Спины БОЛЬШИЕ боковые
 
 spin_big_left = Spinbox(window, from_= 0, to=100, textvariable=myvar_score_left, width=3,
-               font=Font(family='Helvetica', size=36, weight='bold'))
+               font=Font(family='Helvetica', size=36, weight='bold'), command = UpdateSpin)
 spin_big_left.place(x=50, y=100)
     #spin.pack()
 spin_big_right = Spinbox(window, from_=0, to=100, textvariable=myvar_score_right, width=3,
-               font=Font(family='Helvetica', size=36, weight='bold'))
+               font=Font(family='Helvetica', size=36, weight='bold'), command = UpdateSpin)
 spin_big_right.place(x=520, y=100)
 
 
@@ -123,11 +121,11 @@ spin_big_right.place(x=520, y=100)
 #Спины МАЛЕНЬКИЕ посередине
 #Левый
 spin_left = Spinbox(window, from_=0, to=5, textvariable=myvar_set_left, width=3,
-               font=Font(family='Helvetica', size=28, weight='bold'))
+               font=Font(family='Helvetica', size=28, weight='bold'), command = UpdateSpin)
 spin_left.place(x=250, y=50)
 #Правый
 spin_right = Spinbox(window, from_=0, to=5, textvariable=myvar_set_right, width=3,
-               font=Font(family='Helvetica', size=28, weight='bold'))
+               font=Font(family='Helvetica', size=28, weight='bold'), command = UpdateSpin)
 spin_right.place(x=350, y=50)
 
 
